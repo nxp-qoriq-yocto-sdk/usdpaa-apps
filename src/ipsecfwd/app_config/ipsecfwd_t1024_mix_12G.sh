d66c34d1956f778b244d85d1dd4fbb7e6cbb350a
Sandeep Singh <sandeep@freescale.com>
2015-03-18 07:25:39 -0400

    ipsecfwd/app_config: Modified automation scripts for T1024
    Changed scripts to support back-to-back test config
    
    Signed-off-by: Sandeep Singh <sandeep@freescale.com>
    Change-Id: I740fc756d15b06a1cd8eb88ddead907c70e2544e
    Reviewed-on: http://git.am.freescale.net:8181/32936
    Reviewed-by: Vakul Garg <vakul@freescale.com>
    Tested-by: Vakul Garg <vakul@freescale.com>

--- src/ipsecfwd/app_config/ipsecfwd_t1024_mix_12G.sh
+++ src/ipsecfwd/app_config/ipsecfwd_t1024_mix_12G.sh
@@ -22,7 +22,8 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# Script for board_left-board_right ipsecfwd connction of 1G and 10G ports
+# Script for board_left-board_right ipsecfwd connection of two 1G and
+# one 2.5G/10G port
 # Note: check the mac addresses of the board and create ARP entry accordingly
 
 pid=$2
@@ -32,62 +33,97 @@ if [ "$pid" == "" ]
 		exit 1
 fi
 
-ipsecfwd_config -P $pid -F -a 192.168.20.1 -i 3
-ipsecfwd_config -P $pid -F -a 192.168.30.1 -i 4
-ipsecfwd_config -P $pid -F -a 192.168.80.1 -i 81
-
 if [ "$1" == "left" ]
 then
+	ipsecfwd_config -P $pid -F -a 192.168.20.1 -i 3
+	ipsecfwd_config -P $pid -F -a 192.168.30.1 -i 4
+	ipsecfwd_config -P $pid -F -a 192.168.80.1 -i 81
+
     i=2
     while [ "$i" -le 17 ]
     do
 #set the mac address of the right board here for creating ARP entry
       ipsecfwd_config -P $pid -G -s 192.168.20.$i -m 00:e0:0c:00:e2:1e -r true
       ipsecfwd_config -P $pid -G -s 192.168.30.$i -m 00:e0:0c:00:e2:28 -r true
-      ipsecfwd_config -P $pid -G -s 192.168.80.$i -m 00:e0:0c:00:e2:32 -r true
+      ipsecfwd_config -P $pid -G -s 192.168.80.$i -m 00:04:9f:01:02:05 -r true
       i=`expr $i + 1`
     done
     f=out
     s=in
+
+    w=1
+    i=2
+    while [ "$i" -le 17 ]
+    do
+        j=2
+        while [ "$j" -le 17 ]
+        do
+            ipsecfwd_config -P $pid -A -s  192.168.20.$i -d  192.168.40.$j \
+                -g 192.168.20.2 -G 192.168.80.2 -i $w -r $f
+            w=`expr $w + 1`
+            ipsecfwd_config -P $pid -A -s  192.168.30.$i -d  192.168.50.$j \
+                -g 192.168.30.2 -G 192.168.80.2 -i $w -r $f
+            w=`expr $w + 1`
+            ipsecfwd_config -P $pid -A -s  192.168.40.$i -d  192.168.20.$j \
+                -g 192.168.40.2 -G 192.168.80.2 -i $w -r $s
+            w=`expr $w + 1`
+            ipsecfwd_config -P $pid -A -s  192.168.50.$i -d  192.168.30.$j \
+                -g 192.168.50.2 -G 192.168.80.2 -i $w -r $s
+            w=`expr $w + 1`
+
+            j=`expr $j + 1`
+        done
+    i=`expr $i + 1`
+    done
 fi
 
 if [ "$1" == "right" ]
 then
+	ipsecfwd_config -P $pid -F -a 192.168.40.1 -i 3
+	ipsecfwd_config -P $pid -F -a 192.168.50.1 -i 4
+	ipsecfwd_config -P $pid -F -a 192.168.80.1 -i 81
+
     i=2
     while [ "$i" -le 17 ]
     do
 #set the mac address of the left board here for creating ARP entry
-      ipsecfwd_config -P $pid -G -s 192.168.20.$i -m 00:e0:0c:00:8a:1e -r true
-      ipsecfwd_config -P $pid -G -s 192.168.30.$i -m 00:e0:0c:00:8a:28 -r true
-      ipsecfwd_config -P $pid -G -s 192.168.80.$i -m 00:e0:0c:00:8a:32 -r true
+      ipsecfwd_config -P $pid -G -s 192.168.40.$i -m 00:e0:0c:00:8a:1e -r true
+      ipsecfwd_config -P $pid -G -s 192.168.50.$i -m 00:e0:0c:00:8a:28 -r true
+      ipsecfwd_config -P $pid -G -s 192.168.80.$i -m 00:04:9f:11:12:24 -r true
       i=`expr $i + 1`
     done
     f=in
     s=out
+
+    w=1
+    i=2
+    while [ "$i" -le 17 ]
+    do
+        j=2
+        while [ "$j" -le 17 ]
+        do
+            ipsecfwd_config -P $pid -A -s  192.168.20.$i -d  192.168.40.$j \
+                -g 192.168.20.2 -G 192.168.80.2 -i $w -r $f
+            w=`expr $w + 1`
+            ipsecfwd_config -P $pid -A -s  192.168.30.$i -d  192.168.50.$j \
+                -g 192.168.30.2 -G 192.168.80.2 -i $w -r $f
+            w=`expr $w + 1`
+            ipsecfwd_config -P $pid -A -s  192.168.40.$i -d  192.168.20.$j \
+                -g 192.168.40.2 -G 192.168.80.2 -i $w -r $s
+            w=`expr $w + 1`
+            ipsecfwd_config -P $pid -A -s  192.168.50.$i -d  192.168.30.$j \
+                -g 192.168.50.2 -G 192.168.80.2 -i $w -r $s
+            w=`expr $w + 1`
+
+            j=`expr $j + 1`
+        done
+    i=`expr $i + 1`
+    done
 fi
 
 if [ "$f" == "" ]
 then
     exit
 fi
-
-w=1
-i=2
-while [ "$i" -le 23 ]
-do
-    j=2
-    while [ "$j" -le 24 ]
-    do
-        ipsecfwd_config -P $pid -A -s  192.168.20.$i -d  192.168.80.$j \
-            -g 192.168.20.2 -G 192.168.80.2 -i $w -r $f
-        w=`expr $w + 1`
-        ipsecfwd_config -P $pid -A -s  192.168.30.$i -d  192.168.80.$j \
-            -g 192.168.30.2 -G 192.168.80.2 -i $w -r $f
-        w=`expr $w + 1`
-
-        j=`expr $j + 1`
-    done
-    i=`expr $i + 1`
-done
 
 echo IPSecfwd CP initialization complete
