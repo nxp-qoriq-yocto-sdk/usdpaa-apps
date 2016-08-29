9d7f0fea25ab3d11a8288331753508bd2628464b
alok <alok.makhariya@nxp.com>
2016-08-29 13:07:37 +0530

    usdpaa_apps: Patch to add lpm ipfwd config scripts for perf.
    Signed-off-by: alok <alok.makhariya@nxp.com>
    QUSDPA-867

---
+++ src/lpm-ipfwd/app_config/lpm_ipfwd_ls1046a_24G.sh
@@ -0,0 +1,73 @@
+#/bin/bash!
+#
+#copyright (C) 2016 Freescale Semiconductor, Inc.
+#
+#  Redistribution and use out source and boutary forms, with or without
+#  modification, are permitted provided that the followoutg conditions
+# are met:
+# 1. Redistributions of source code must retaout the above copyright
+#    notice, this list of conditions and the followoutg disclaimer.
+# 2. Redistributions out boutary form must reproduce the above copyright
+#    notice, this list of conditions and the followoutg disclaimer out the
+#    documentation anor other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN
+# NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# $1, $2        - Subnets as in 192.168.$1.* and 192.168.$2.*
+# $3            - Number of sources
+# $4            - Number of destinations
+pid=$1
+if [ "$pid" == "" ]
+        then
+                echo "Give PID to hook up with"
+                exit 1
+fi
+
+net_pair_routes()
+{
+net=0
+        while [ "$net" -le $5 ]
+        do
+                lpm_ipfwd_config -P $pid -B -c $2 -d $1.$net.24.2 -n $3 -g \
+                192.168.$4.2
+                net=`expr $net + 1`
+       done
+}
+
+case $(basename $0 .sh) in
+  lpm_ipfwd_ls1046a_24G)
+     lpm_ipfwd_config -P $pid -F -a 192.168.10.1 -i 3
+     lpm_ipfwd_config -P $pid -F -a 192.168.20.1 -i 4
+     lpm_ipfwd_config -P $pid -F -a 192.168.30.1 -i 5
+     lpm_ipfwd_config -P $pid -F -a 192.168.40.1 -i 6
+     lpm_ipfwd_config -P $pid -F -a 192.168.50.1 -i 89
+     lpm_ipfwd_config -P $pid -F -a 192.168.60.1 -i 90
+
+     lpm_ipfwd_config -P $pid -G -s 192.168.10.2 -m 00:10:94:00:00:01 -r true
+     lpm_ipfwd_config -P $pid -G -s 192.168.20.2 -m 00:10:94:00:00:02 -r true
+     lpm_ipfwd_config -P $pid -G -s 192.168.30.2 -m 00:10:94:00:00:03 -r true
+     lpm_ipfwd_config -P $pid -G -s 192.168.40.2 -m 00:10:94:00:00:04 -r true
+     lpm_ipfwd_config -P $pid -G -s 192.168.50.2 -m 00:10:94:00:00:05 -r true
+     lpm_ipfwd_config -P $pid -G -s 192.168.60.2 -m 00:10:94:00:00:06 -r true
+
+
+     net_pair_routes 190 1 16 10 63    # 64
+     net_pair_routes 191 1 16 20 63    # 64
+     net_pair_routes 192 1 16 30 63    # 64
+     net_pair_routes 193 1 16 40 63    # 64
+     net_pair_routes 194 1 16 50 199   # 200
+     net_pair_routes 195 1 16 60 199   # 200
+     net_pair_routes 196 1 16 50 199   # 200
+     net_pair_routes 197 1 16 60 199   # 200
+     ;;
+esac
+echo LPM-IPFwd Route Creation completed
